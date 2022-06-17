9ac50a3164fe476460e42a4530c07898752a2e75
liujiajun <45444154+wo1261931780@users.noreply.github.com>
2022-06-17 22:38:15 +0800

    init

---
+++ src/test/java/com/example/stcodeClips/github/io/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stcodeClips.github.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
