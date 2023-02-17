d200d1c1dc9b5cc0b370f4014a8c5c716cc6c9cf
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-17 17:12:45 +0800

    整理代码

--- src/main/java/com/example/stcodeClips/github/io/a20230217/A005出题器.java
+++ src/main/java/com/example/stcodeClips/github/io/a20230217/A005出题器.java
@@ -3,6 +3,7 @@ package com.example.stcodeClips.github.io.a20230217;
 import lombok.extern.slf4j.Slf4j;
 
 import java.security.SecureRandom;
+
 /**
  * @author junw
  */
@@ -10,9 +11,9 @@ import java.security.SecureRandom;
 @Slf4j
 public class A005出题器 {
 	public static void main(String[] args) {
-		// Random x = new Random();
 		SecureRandom x = new SecureRandom();
-		for (int i = 0; i < 3; i++) {
+		int mid = 3;
+		for (int i = 0; i < mid; i++) {
 			int x1 = x.nextInt(44);
 			log.info(String.valueOf(x1));
 		}
