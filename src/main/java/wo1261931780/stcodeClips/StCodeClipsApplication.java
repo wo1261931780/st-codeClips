10d7cfa360034b804424c513ec11540349aebf96
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-26 20:48:27 +0800

    2

---
+++ src/main/java/wo1261931780/stcodeClips/StCodeClipsApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.stcodeClips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StCodeClipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StCodeClipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/stcodeClips/StCodeClipsApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.stcodeClips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StCodeClipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
